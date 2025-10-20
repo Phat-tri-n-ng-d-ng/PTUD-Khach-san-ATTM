5acc9585c34218468bc006c227c3f01fc9302c10
manhkl1812 <manhkl1025@gmail.com>
2025-10-21 02:20:25 +0700

    Giao dien Hoa Doc + CRUD

---
+++ PTUD_Khach_san_ATTM/src/database/HoaDonDao.java
@@ -0,0 +1,26 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import entity.HoaDon;
+
+public class HoaDonDao {
+	public ArrayList<HoaDon> getDanhSachHoaDon() {
+		Connection con = connectDB.getConnection();
+		ArrayList<HoaDon> danhSachHoaDon = new ArrayList<>();
+		try {
+			PreparedStatement st = con.prepareStatement();
+			ResultSet rs = st.executeQuery("Select maHD  from HoaDon");
+			while (rs.next()) {
+				
+			}
+				
+		} catch (Exception e) {
+			// TODO: handle exception
+		};
+
+}
