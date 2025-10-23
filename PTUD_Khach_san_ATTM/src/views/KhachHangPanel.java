2f653ac489ce099dd7fd31a61687d92dcf84f610
Bui Van Tai <buivantai24122004@gmail.com>
2025-10-23 19:29:24 +0700

    Thêm sự kiện nhân viên

--- PTUD_Khach_san_ATTM/src/views/KhachHangPanel.java
+++ PTUD_Khach_san_ATTM/src/views/KhachHangPanel.java
@@ -1,13 +1,21 @@
 package views;
 
+import controller.KhachHangController;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
+import com.toedter.calendar.JDateChooser;
 import java.awt.*;
 
 public class KhachHangPanel extends JPanel {
+    public JRadioButton rdbtn_Nu;
+    public JRadioButton rdbtn_Nam;
+    public JDateChooser ngaySinh;
+    public JTextField txt_soCCCD;
+    private KhachHangController khachHangController;
     public JButton btn_Tim;
     public JButton btn_ThemKhachHang;
     public JButton btn_CapNhat;
@@ -28,110 +36,139 @@ public class KhachHangPanel extends JPanel {
         setBorder(new EmptyBorder(5, 5, 5, 5));
         setLayout(null);
 
-        JLabel lbl_TieuDe = new JLabel("Khách Hàng");
-        lbl_TieuDe.setForeground(new Color(10, 100, 189));
-        lbl_TieuDe.setFont(new Font("Times New Roman", Font.BOLD, 24));
-        lbl_TieuDe.setBounds(725, 10, 133, 29);
-        add(lbl_TieuDe);
+        JLabel lpl_TieuDe = new JLabel("Khách Hàng");
+        lpl_TieuDe.setForeground(new Color(10, 100, 189));
+        lpl_TieuDe.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        lpl_TieuDe.setBounds(725, 10, 133, 29);
+        add(lpl_TieuDe);
 
-        JPanel pnlThongTinNhanVien = new JPanel();
-        pnlThongTinNhanVien.setBackground(new Color(255, 255, 255));
-        pnlThongTinNhanVien.setBorder(new LineBorder(new Color(0, 0, 0)));
+        JPanel pnlThongTinKhachHang = new JPanel();
+        pnlThongTinKhachHang.setBackground(new Color(255, 255, 255));
+        pnlThongTinKhachHang.setBorder(new LineBorder(new Color(0, 0, 0)));
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize(1536, 818);
         int doDaiThongTinNhanVien = screenSize.width - 40;
         int viChiDauThongTinNhanVien = (screenSize.width - doDaiThongTinNhanVien) / 2;
-        pnlThongTinNhanVien.setBounds(20, 82,1496 , 175);
-        add(pnlThongTinNhanVien);
-        pnlThongTinNhanVien.setLayout(null);
+
+        pnlThongTinKhachHang.setBounds(20, 79,1496 , 190);
+        add(pnlThongTinKhachHang);
+        pnlThongTinKhachHang.setLayout(null);
 
         JLabel lpl_TenNhanVien = new JLabel("Tên khách hàng:");
         lpl_TenNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lpl_TenNhanVien.setBounds(10, 10, 104, 13);
-        pnlThongTinNhanVien.add(lpl_TenNhanVien);
+        lpl_TenNhanVien.setBounds(10, 10, 104, 16);
+        pnlThongTinKhachHang.add(lpl_TenNhanVien);
 
         txt_TenKhachHang = new JTextField();
         txt_TenKhachHang.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         int doDaiTxt = doDaiThongTinNhanVien/2 - 20;
         txt_TenKhachHang.setBounds(10, 33, doDaiTxt, 30);
-        pnlThongTinNhanVien.add(txt_TenKhachHang);
+        pnlThongTinKhachHang.add(txt_TenKhachHang);
         txt_TenKhachHang.setColumns(10);
 
         txt_SoDienThoai = new JTextField();
         txt_SoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         txt_SoDienThoai.setColumns(10);
         txt_SoDienThoai.setBounds(doDaiTxt + 30, 33, doDaiTxt, 30);
-        pnlThongTinNhanVien.add(txt_SoDienThoai);
+        pnlThongTinKhachHang.add(txt_SoDienThoai);
 
         JLabel lbl_SoDienThoai = new JLabel("Số điện thoại:");
         lbl_SoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_SoDienThoai.setBounds(758, 11, 104, 13);
-        pnlThongTinNhanVien.add(lbl_SoDienThoai);
+        lbl_SoDienThoai.setBounds(758, 11, 104, 16);
+        pnlThongTinKhachHang.add(lbl_SoDienThoai);
 
         JLabel lpl_GioiTinh = new JLabel("Giới tính:");
         lpl_GioiTinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lpl_GioiTinh.setBounds(10, 73, 104, 13);
-        pnlThongTinNhanVien.add(lpl_GioiTinh);
+        lpl_GioiTinh.setBounds(10, 134, 104, 16);
+        pnlThongTinKhachHang.add(lpl_GioiTinh);
 
-        JRadioButton rdbtn_Nam = new JRadioButton("Nam");
+        rdbtn_Nam = new JRadioButton("Nam");
         rdbtn_Nam.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         rdbtn_Nam.setSelected(true);
         rdbtn_Nam.setBackground(new Color(255, 255, 255));
-        rdbtn_Nam.setBounds(10, 92, 65, 21);
-        pnlThongTinNhanVien.add(rdbtn_Nam);
+        rdbtn_Nam.setBounds(10, 153, 65, 21);
+        pnlThongTinKhachHang.add(rdbtn_Nam);
 
-        JRadioButton rdbtn_Nu = new JRadioButton("Nữ");
+        rdbtn_Nu = new JRadioButton("Nữ");
         rdbtn_Nu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         rdbtn_Nu.setBackground(Color.WHITE);
-        rdbtn_Nu.setBounds(75, 93, 47, 21);
-        pnlThongTinNhanVien.add(rdbtn_Nu);
+        rdbtn_Nu.setBounds(77, 153, 47, 21);
+        pnlThongTinKhachHang.add(rdbtn_Nu);
+
+        ButtonGroup groupGioiTinh = new ButtonGroup();
+        groupGioiTinh.add(rdbtn_Nam);
+        groupGioiTinh.add(rdbtn_Nu);
 
         JLabel lbl_NgaySinh = new JLabel("Ngày sinh:");
         lbl_NgaySinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_NgaySinh.setBounds(223, 69, 104, 20);
-        pnlThongTinNhanVien.add(lbl_NgaySinh);
+        lbl_NgaySinh.setBounds(130, 134, 104, 16);
+        pnlThongTinKhachHang.add(lbl_NgaySinh);
+
+        ngaySinh = new JDateChooser();
+        ngaySinh.setDateFormatString("dd/MM/yyyy");
+        ngaySinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        ngaySinh.setBounds(130, 153, 173, 30);
+        pnlThongTinKhachHang.add(ngaySinh);
 
         JLabel lbl_Email = new JLabel("Email:");
         lbl_Email.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_Email.setBounds(758, 74, 47, 13);
-        pnlThongTinNhanVien.add(lbl_Email);
+        lbl_Email.setBounds(758, 74, 47, 16);
+        pnlThongTinKhachHang.add(lbl_Email);
 
         txt_Email = new JTextField();
         txt_Email.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         txt_Email.setColumns(10);
         txt_Email.setBounds(758, 94, 728, 30);
-        pnlThongTinNhanVien.add(txt_Email);
+        pnlThongTinKhachHang.add(txt_Email);
 
         btn_ThemKhachHang = new JButton("Thêm KH");
-        btn_ThemKhachHang.setBounds(1012, 134, 120, 30);
-        pnlThongTinNhanVien.add(btn_ThemKhachHang);
+        btn_ThemKhachHang.setBounds(1012, 149, 120, 30);
+        pnlThongTinKhachHang.add(btn_ThemKhachHang);
 
         btn_CapNhat = new JButton("Cập nhật");
-        btn_CapNhat.setBounds(1142, 134, 120, 30);
-        pnlThongTinNhanVien.add(btn_CapNhat);
+        btn_CapNhat.setBounds(1142, 149, 120, 30);
+        pnlThongTinKhachHang.add(btn_CapNhat);
+
+        JLabel lbl_soCCCD = new JLabel("Số căn cước công dân:");
+        lbl_soCCCD.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        lbl_soCCCD.setBounds(10, 74, 161, 16);
+        pnlThongTinKhachHang.add(lbl_soCCCD);
+
+        txt_soCCCD = new JTextField();
+        txt_soCCCD.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        txt_soCCCD.setColumns(10);
+        txt_soCCCD.setBounds(10, 94, 728, 30);
+        pnlThongTinKhachHang.add(txt_soCCCD);
 
         JPanel pnlBoLoc = new JPanel();
         pnlBoLoc.setBorder(new LineBorder(new Color(0, 0, 0)));
         pnlBoLoc.setBackground(new Color(255, 255, 255));
-        pnlBoLoc.setBounds(20, 297, 1496, 73);
+        pnlBoLoc.setBounds(20, 309, 1496, 73);
         add(pnlBoLoc);
         pnlBoLoc.setLayout(null);
 
-        JLabel lbl_ = new JLabel("Tìm tên khách hàng:");
-        lbl_.setBounds(10, 10, 126, 20);
-        lbl_.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        pnlBoLoc.add(lbl_);
+        JRadioButton rdbtn_TimTenKhachHang = new JRadioButton("Tìm tên khách hàng:");
+        rdbtn_TimTenKhachHang.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        rdbtn_TimTenKhachHang.setBackground(Color.WHITE);
+        rdbtn_TimTenKhachHang.setBounds(10, 9, 159, 16);
+        rdbtn_TimTenKhachHang.setSelected(true);
+        pnlBoLoc.add(rdbtn_TimTenKhachHang);
+
+        JRadioButton rdbtn_TimSoDienThoai = new JRadioButton("Tìm số điện thoại:");
+        rdbtn_TimSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        rdbtn_TimSoDienThoai.setBackground(Color.WHITE);
+        rdbtn_TimSoDienThoai.setBounds(646, 11, 159, 16);
+        pnlBoLoc.add(rdbtn_TimSoDienThoai);
+
+        ButtonGroup groupTimKiem = new ButtonGroup();
+        groupTimKiem.add(rdbtn_TimTenKhachHang);
+        groupTimKiem.add(rdbtn_TimSoDienThoai);
 
         txt_TimTenNhanVien = new JTextField();
         txt_TimTenNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         txt_TimTenNhanVien.setColumns(10);
         txt_TimTenNhanVien.setBounds(10, 33, 616, 30);
         pnlBoLoc.add(txt_TimTenNhanVien);
-
-        JLabel lbl_TmSoDienThoai = new JLabel("Tìm số điện thoại:");
-        lbl_TmSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_TmSoDienThoai.setBounds(646, 10, 117, 20);
-        pnlBoLoc.add(lbl_TmSoDienThoai);
 
         txt_TimSoDienThoai = new JTextField();
         txt_TimSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
@@ -148,48 +185,47 @@ public class KhachHangPanel extends JPanel {
         lbl_HangKhachHang.setBounds(1382, 10, 114, 20);
         pnlBoLoc.add(lbl_HangKhachHang);
 
-        JComboBox cbb_LocChuVu = new JComboBox();
-        cbb_LocChuVu.setModel(new DefaultComboBoxModel(new String[] {"Tất cả", "Đồng", "Bạc", "Vàng", "Kim cương"}));
-        cbb_LocChuVu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        cbb_LocChuVu.setBounds(1382, 33, 104, 30);
-        pnlBoLoc.add(cbb_LocChuVu);
+        JComboBox cbb_LocHangKhachHang = new JComboBox();
+        cbb_LocHangKhachHang.setModel(new DefaultComboBoxModel(new String[] {"Tất cả", "Đồng", "Bạc", "Vàng", "Kim cương"}));
+        cbb_LocHangKhachHang.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        cbb_LocHangKhachHang.setBounds(1382, 33, 104, 30);
+        pnlBoLoc.add(cbb_LocHangKhachHang);
 
-        JLabel lbl_ThongTinKhachHang = new JLabel("Thông tin khách hàng");
+        JLabel lbl_ThongTinKhachHang = new JLabel("Thông Tin Khách Hàng");
         lbl_ThongTinKhachHang.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        lbl_ThongTinKhachHang.setBounds(20, 52, 180, 20);
+        lbl_ThongTinKhachHang.setBounds(20, 49, 190, 20);
         add(lbl_ThongTinKhachHang);
 
-        JLabel lbl_BoLoc = new JLabel("Bộ lọc");
+        JLabel lbl_BoLoc = new JLabel("Bộ Lọc");
         lbl_BoLoc.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        lbl_BoLoc.setBounds(20, 267, 58, 20);
+        lbl_BoLoc.setBounds(20, 279, 70, 20);
         add(lbl_BoLoc);
 
-        JLabel lbl_DanhSachNhanVien = new JLabel("Danh Sách Nhân Viên");
-        lbl_DanhSachNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        lbl_DanhSachNhanVien.setBounds(20, 380, 180, 20);
-        add(lbl_DanhSachNhanVien);
+        JLabel lbl_DanhSachKhachHang = new JLabel("Danh Sách Khách Hàng");
+        lbl_DanhSachKhachHang.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lbl_DanhSachKhachHang.setBounds(20, 392, 200, 21);
+        add(lbl_DanhSachKhachHang);
 
-        JPanel pnlDanhSachNhanVien = new JPanel();
-        pnlDanhSachNhanVien.setBackground(new Color(255, 255, 255));
-        pnlDanhSachNhanVien.setBorder(new LineBorder(new Color(0, 0, 0)));
-        pnlDanhSachNhanVien.setBounds(20, 410, 1496, 375);
-        add(pnlDanhSachNhanVien);
+        JPanel pnlDanhSachKhachHang = new JPanel();
+        pnlDanhSachKhachHang.setBackground(new Color(255, 255, 255));
+        pnlDanhSachKhachHang.setBorder(new LineBorder(new Color(0, 0, 0)));
+        pnlDanhSachKhachHang.setBounds(20, 422, 1496, 369);
+        add(pnlDanhSachKhachHang);
 
-        model = new DefaultTableModel(new String[] {"Mã khách hàng","Tên khách hàng","Giới tính","Ngày sinh","Số điện thoại","Email","Hạng khách hàng","Điểm tích lũy"}, 0);
-        pnlDanhSachNhanVien.setLayout(null);
+        model = new DefaultTableModel(new String[] {"Mã khách hàng","Tên khách hàng","Giới tính","Ngày sinh","Số điện thoại","Email","Số CCCD","Hạng khách hàng","Điểm tích lũy"}, 0);
+        pnlDanhSachKhachHang.setLayout(null);
         table = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(10, 10, 1476, 355);
+        scrollPane.setBounds(10, 10, 1476, 349);
         JTableHeader header = table.getTableHeader();
         header.setFont(new Font("Times New Roman", Font.BOLD, 18));
         header.setBackground(new Color(10, 100, 189));
         header.setForeground(Color.WHITE);
         header.setPreferredSize(new Dimension(header.getWidth(), 35));
         table.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        pnlDanhSachNhanVien.add(scrollPane);
+        pnlDanhSachKhachHang.add(scrollPane);
 
-        JPanel panel = new JPanel();
-        panel.setBounds(10, 10, 32, 32);
-        add(panel);
+        khachHangController = new KhachHangController(this);
+        khachHangController.getTatCaKhachHang();
     }
 }
--- PTUD_Khach_san_ATTM/src/views/NhanVienPanel.java
+++ PTUD_Khach_san_ATTM/src/views/NhanVienPanel.java
@@ -6,8 +6,17 @@ import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.toedter.calendar.JDateChooser;
+import controller.NhanVienController;
 
 public class NhanVienPanel extends JPanel{
+    public JComboBox cbb_LocChucVu;
+    public JRadioButton rdbtn_Nam;
+    public JRadioButton rdbtn_Nu;
+    private NhanVienController nhanVienController;
     public JButton btn_Tim;
     public JButton btn_CapTaiKhoan;
     public JButton btn_ThemNhanVien;
@@ -33,6 +42,11 @@ public class NhanVienPanel extends JPanel{
         lbl_TieuDe.setBounds(725, 10, 114, 24);
         add(lbl_TieuDe);
 
+        JLabel lbl_ThongTinNhanVien = new JLabel("Thông Tin Nhân Viên");
+        lbl_ThongTinNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lbl_ThongTinNhanVien.setBounds(20, 49, 180, 20);
+        add(lbl_ThongTinNhanVien);
+
         JPanel pnlThongTinNhanVien = new JPanel();
         pnlThongTinNhanVien.setBackground(new Color(255, 255, 255));
         pnlThongTinNhanVien.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -40,13 +54,13 @@ public class NhanVienPanel extends JPanel{
         setSize(screenSize.width, screenSize.height);
         int doDaiThongTinNhanVien = screenSize.width - 40;
         int viChiDauThongTinNhanVien = (screenSize.width - doDaiThongTinNhanVien) / 2;
-        pnlThongTinNhanVien.setBounds(20, 64,1496 , 175);
+        pnlThongTinNhanVien.setBounds(20, 79,1496 , 175);
         add(pnlThongTinNhanVien);
         pnlThongTinNhanVien.setLayout(null);
 
         JLabel lpl_TenNhanVien = new JLabel("Tên nhân viên:");
         lpl_TenNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lpl_TenNhanVien.setBounds(10, 10, 104, 13);
+        lpl_TenNhanVien.setBounds(10, 10, 104, 16);
         pnlThongTinNhanVien.add(lpl_TenNhanVien);
 
         txt_TenNhanVien = new JTextField();
@@ -63,25 +77,25 @@ public class NhanVienPanel extends JPanel{
 
         JLabel lbl_SoDienThoai = new JLabel("Số điện thoại:");
         lbl_SoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_SoDienThoai.setBounds(758, 11, 104, 13);
+        lbl_SoDienThoai.setBounds(758, 11, 104, 16);
         pnlThongTinNhanVien.add(lbl_SoDienThoai);
 
         JLabel lpl_GioiTinh = new JLabel("Giới tính:");
         lpl_GioiTinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lpl_GioiTinh.setBounds(10, 73, 104, 13);
+        lpl_GioiTinh.setBounds(10, 73, 104, 16);
         pnlThongTinNhanVien.add(lpl_GioiTinh);
 
-        JRadioButton rdbtn_Nam = new JRadioButton("Nam");
+        rdbtn_Nam = new JRadioButton("Nam");
         rdbtn_Nam.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         rdbtn_Nam.setSelected(true);
         rdbtn_Nam.setBackground(new Color(255, 255, 255));
-        rdbtn_Nam.setBounds(10, 92, 65, 21);
+        rdbtn_Nam.setBounds(8, 103, 65, 21);
         pnlThongTinNhanVien.add(rdbtn_Nam);
 
-        JRadioButton rdbtn_Nu = new JRadioButton("Nữ");
+        rdbtn_Nu = new JRadioButton("Nữ");
         rdbtn_Nu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         rdbtn_Nu.setBackground(Color.WHITE);
-        rdbtn_Nu.setBounds(75, 93, 47, 21);
+        rdbtn_Nu.setBounds(75, 103, 47, 21);
         pnlThongTinNhanVien.add(rdbtn_Nu);
 
         ButtonGroup groupGioiTinh = new ButtonGroup();
@@ -90,12 +104,18 @@ public class NhanVienPanel extends JPanel{
 
         JLabel lbl_NgaySinh = new JLabel("Ngày sinh:");
         lbl_NgaySinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_NgaySinh.setBounds(124, 73, 104, 13);
+        lbl_NgaySinh.setBounds(128, 73, 104, 16);
         pnlThongTinNhanVien.add(lbl_NgaySinh);
+
+        JDateChooser ngaySinh = new JDateChooser();
+        ngaySinh.setDateFormatString("dd/MM/yyyy");
+        ngaySinh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        ngaySinh.setBounds(128, 99, 173, 30);
+        pnlThongTinNhanVien.add(ngaySinh);
 
         JLabel lbl_Email = new JLabel("Email:");
         lbl_Email.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_Email.setBounds(758, 74, 47, 13);
+        lbl_Email.setBounds(758, 74, 47, 16);
         pnlThongTinNhanVien.add(lbl_Email);
 
         txt_Email = new JTextField();
@@ -117,36 +137,26 @@ public class NhanVienPanel extends JPanel{
 
         JLabel lbl_ChucVu = new JLabel("Chức vụ:");
         lbl_ChucVu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_ChucVu.setBounds(290, 73, 104, 13);
+        lbl_ChucVu.setBounds(311, 73, 104, 16);
         pnlThongTinNhanVien.add(lbl_ChucVu);
 
         cbb_ChuVu = new JComboBox();
         cbb_ChuVu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        cbb_ChuVu.setModel(new DefaultComboBoxModel(new String[] {"Quản lý", "Lễ tân", "Kế toán", "Kỹ thuật", "Buồng phòng", "Bếp", "Bảo Vệ"}));
-        cbb_ChuVu.setBounds(290, 92, 104, 30);
+        cbb_ChuVu.setModel(new DefaultComboBoxModel(new String[] {"Quản lý", "Lễ tân", "Kế toán", "Kỹ thuật", "Buồng phòng", "Bếp", "Bảo vệ"}));
+        cbb_ChuVu.setBounds(311, 99, 104, 30);
         pnlThongTinNhanVien.add(cbb_ChuVu);
 
         JPanel pnlBoLoc = new JPanel();
         pnlBoLoc.setBorder(new LineBorder(new Color(0, 0, 0)));
         pnlBoLoc.setBackground(new Color(255, 255, 255));
-        pnlBoLoc.setBounds(20, 279, 1496, 73);
+        pnlBoLoc.setBounds(20, 294, 1496, 73);
         add(pnlBoLoc);
         pnlBoLoc.setLayout(null);
-
-        JLabel lbl_TimTenNhanVien = new JLabel("Tìm tên nhân viên:");
-        lbl_TimTenNhanVien.setBounds(10, 10, 117, 20);
-        lbl_TimTenNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        pnlBoLoc.add(lbl_TimTenNhanVien);
 
         txt_TimTenNhanVien = new JTextField();
         txt_TimTenNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         txt_TimTenNhanVien.setBounds(10, 33, 616, 30);
         pnlBoLoc.add(txt_TimTenNhanVien);
-
-        JLabel lbl_TmSoDienThoai = new JLabel("Tìm số điện thoại:");
-        lbl_TmSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        lbl_TmSoDienThoai.setBounds(646, 10, 117, 20);
-        pnlBoLoc.add(lbl_TmSoDienThoai);
 
         txt_TimSoDienThoai = new JTextField();
         txt_TimSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
@@ -162,33 +172,50 @@ public class NhanVienPanel extends JPanel{
         lbl_ChucVu_1.setBounds(1382, 10, 104, 20);
         pnlBoLoc.add(lbl_ChucVu_1);
 
-        JComboBox cbb_LocChuVu = new JComboBox();
-        cbb_LocChuVu.setModel(new DefaultComboBoxModel(new String[] {"Tất cả", "Quản lý", "Lễ tân", "Kế toán", "Kỹ thuật", "Buồng phòng", "Bếp", "Bảo Vệ"}));
-        cbb_LocChuVu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-        cbb_LocChuVu.setBounds(1382, 33, 104, 30);
-        pnlBoLoc.add(cbb_LocChuVu);
+        cbb_LocChucVu = new JComboBox();
+        cbb_LocChucVu.setModel(new DefaultComboBoxModel(new String[] {"Tất cả", "Quản lý", "Lễ tân", "Kế toán", "Kỹ thuật", "Buồng phòng", "Bếp", "Bảo vệ"}));
+        cbb_LocChucVu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        cbb_LocChucVu.setBounds(1382, 33, 104, 30);
+        pnlBoLoc.add(cbb_LocChucVu);
 
-        JLabel lbl_BoLoc = new JLabel("Bộ lọc");
+        JRadioButton rdbtn_TimMaNhanVien = new JRadioButton("Tìm mã nhân viên");
+        rdbtn_TimMaNhanVien.setSelected(true);
+        rdbtn_TimMaNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        rdbtn_TimMaNhanVien.setBackground(Color.WHITE);
+        rdbtn_TimMaNhanVien.setBounds(10, 11, 137, 21);
+        pnlBoLoc.add(rdbtn_TimMaNhanVien);
+
+        JRadioButton rdbtn_TimSoDienThoai = new JRadioButton("Tìm số điện thoại");
+        rdbtn_TimSoDienThoai.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        rdbtn_TimSoDienThoai.setBackground(Color.WHITE);
+        rdbtn_TimSoDienThoai.setBounds(646, 6, 137, 21);
+        pnlBoLoc.add(rdbtn_TimSoDienThoai);
+
+        ButtonGroup groupTimKiem = new ButtonGroup();
+        groupTimKiem.add(rdbtn_TimMaNhanVien);
+        groupTimKiem.add(rdbtn_TimSoDienThoai);
+
+        JLabel lbl_BoLoc = new JLabel("Bộ Lọc");
         lbl_BoLoc.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        lbl_BoLoc.setBounds(20, 249, 58, 20);
+        lbl_BoLoc.setBounds(20, 264, 70, 20);
         add(lbl_BoLoc);
 
         JLabel lbl_DanhSachNhanVien = new JLabel("Danh Sách Nhân Viên");
         lbl_DanhSachNhanVien.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        lbl_DanhSachNhanVien.setBounds(20, 362, 180, 20);
+        lbl_DanhSachNhanVien.setBounds(20, 377, 180, 20);
         add(lbl_DanhSachNhanVien);
 
         JPanel pnlDanhSachNhanVien = new JPanel();
         pnlDanhSachNhanVien.setBackground(new Color(255, 255, 255));
         pnlDanhSachNhanVien.setBorder(new LineBorder(new Color(0, 0, 0)));
-        pnlDanhSachNhanVien.setBounds(20, 395, 1496, 422);
+        pnlDanhSachNhanVien.setBounds(20, 407, 1496, 384);
         add(pnlDanhSachNhanVien);
 
         model = new DefaultTableModel(new String[] {"Mã nhân viên","Tên nhân viên","Giới tính","Ngày sinh","Số điện thoại","Email","Chức vụ"}, 0);
         pnlDanhSachNhanVien.setLayout(null);
         table = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(10, 10, 1476, 402);
+        scrollPane.setBounds(10, 10, 1476, 364);
         JTableHeader header = table.getTableHeader(); // chỉnh sửa header
         header.setFont(new Font("Times New Roman", Font.BOLD, 18));
         header.setBackground(new Color(10, 100, 189));
@@ -197,5 +224,7 @@ public class NhanVienPanel extends JPanel{
         table.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         pnlDanhSachNhanVien.add(scrollPane);
 
+        nhanVienController = new NhanVienController(this);
+        nhanVienController.getTatCaNhanVien();;
     }
 }
